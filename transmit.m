7a892a2085ca2be1c88d4b2821fee06a526cd656
BartCoonen <bartcoonen@hotmail.com>
2017-03-08 13:31:53 +0100

    Delete transmit.m

--- transmit.m
+++
@@ -1,9 +0,0 @@
-function [s] = transmit(t, fc, sBase)
-%CREATETRANSMITTER creates a transmitter transmitting a 101010 bitstream
-%with bitrate Rb with baseband frequency fb and carrier frequency fc. If fb
-%is not given the BPSK will use the carrier for the shift keying.
-
-sCar = createCarrier(t,fc,0);
-s = sCar .* sBase;
-end
-
